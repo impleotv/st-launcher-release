b5438bbbf8ee86ef720fd384b895ec209ed502ac
impleotv <alexc@impleotv.com>
2022-04-19 13:33:30 +0300

    dev

--- index.js
+++ index.js
@@ -8,14 +8,10 @@ if (args.length === 0) {
   console.log('Please enter a version number');
 }
 
-const pjsonServer = JSON.parse(fs.readFileSync('./components/package-server.json', 'utf8'));
-const pjsonFrontend = JSON.parse(fs.readFileSync('./components/package-frontend.json', 'utf8'));
-const pjsonFrontendComp = JSON.parse(fs.readFileSync('./components/package-frontend-comp.json', 'utf8'));
+const pjsonServer = JSON.parse(fs.readFileSync('./st-launcher/package-server.json', 'utf8'));
 
 let DATA = {
   version: pjsonServer.version,
-  frontendVersion: pjsonFrontend.version,
-  frontendCompVersion: pjsonFrontendComp.version,
   date: new Date().toLocaleDateString('en-IL', {
     weekday: 'short',
     month: 'short',
@@ -35,14 +31,4 @@ function generateReadMe() {
   });
 }
 
-function generateVersionsFile() {
-    const output = {
-      latest: pjsonServer.version,
-      date: new Date().toISOString(),
-      releases_link: "https://github.com/impleotv/stserver-release"
-    };
-    fs.writeFileSync('versions.json', JSON.stringify(output));
-}
-
 generateReadMe();
-generateVersionsFile();
